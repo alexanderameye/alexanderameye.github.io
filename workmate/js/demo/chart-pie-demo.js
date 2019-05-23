0c1dc43e991e77fc6cce5e53efb464e137b0d210
Alexander Ameye <alexanderameye@gmail.com>
2019-05-23 13:24:01 +0200

    Added response results

--- workmate/js/demo/chart-pie-demo.js
+++ workmate/js/demo/chart-pie-demo.js
@@ -3,33 +3,79 @@ Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,Bli
 Chart.defaults.global.defaultFontColor = '#858796';
 
 // Pie Chart Example
-var ctx = document.getElementById("myPieChart");
+var ctx = document.getElementById("voteResults");
+
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
+
 var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
+    type: 'doughnut',
+    data: {
+        labels: [],
+        datasets: [{
+            data: [],
+            backgroundColor: ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099', '#3B3EAC', '##0099C6'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
     },
-    legend: {
-      display: false
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: true,
+            position: "left"
+        },
+        cutoutPercentage: 80,
     },
-    cutoutPercentage: 80,
-  },
+
 });
+
+
+var a = document.getElementById("questionTitles");
+a.onchange = function() {
+    $.getJSON("https://studev.groept.be/api/a18_sd308/GetVoteResults/" + a.options[a.selectedIndex].value + "/.php", function(data) {
+        myPieChart.data.labels = [];
+        myPieChart.data.datasets[0].data = [];
+        myPieChart.update();
+
+        for (var i = 0; i < data.length; i++) {
+            addData(myPieChart, data[i]["text"], data[i]["COUNT(text)"]);
+        }
+    });
+}
+
+function showFirstResult() {
+    var a = document.getElementById("questionTitles");
+    $.getJSON("https://studev.groept.be/api/a18_sd308/GetVoteResults/" + a.options[0].value + "/.php", function(data) {
+        myPieChart.data.labels = [];
+        myPieChart.data.datasets[0].data = [];
+        myPieChart.update();
+
+        for (var i = 0; i < data.length; i++) {
+            addData(myPieChart, data[i]["text"], data[i]["COUNT(text)"]);
+        }
+    });
+}

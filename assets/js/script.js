2c7f986c644bf12d53ba2a17196f18f1c1ea5f69
alexanderameye <alexanderameye@gmail.com>
2023-02-24 21:32:23 +0000

    Update from https://github.com/alexanderameye/personal-site/commit/ed1abab85697bdc399b26a92c68209351ca8d940

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+ const observer = new IntersectionObserver( 
+  ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
+  {threshold: [1]}
+);
+
+
+observer.observe(document.querySelector('#navbar-desktop'));
+

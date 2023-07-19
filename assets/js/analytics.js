8738f3cb282d39ebe20e825e52af4e05073d8f89
alexanderameye <alexanderameye@gmail.com>
2023-07-19 13:29:19 +0000

    Update from https://github.com/alexanderameye/personal-site/commit/ae3f6a811e290e509e2015d011a26670ad0d3d7f

--- assets/js/analytics.js
+++
@@ -1,7 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'UA-137365487-1');
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 
- const observer = new IntersectionObserver( 
+ const observer = new IntersectionObserver(
   ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
   {threshold: [1]}
 );
 
 
 observer.observe(document.querySelector('#navbar-desktop'));
-
